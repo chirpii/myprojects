837ca5b405a1b065095d0e03fb6bd999e211335e
Ivan <144043312+ivanchirpii@users.noreply.github.com>
2023-09-07 13:46:26 +0300

    Loan_calculator.py

--- Loan_calculator.py
+++ Loan_calculator.py
@@ -1,16 +1,12 @@
 print('\033[1;34;48m === INTEREST CALCULATOR === \033[0m')
-sum = 0
-period = 0
-percentage = 0
+print()
 total_interest = 0
 sum = float(input('Introduce loan sum: '))
 period = int(input('Introduce period (In years): '))
 percentage = float(input('Introduce annualy interest (in %): ')) / 100
 for num in range(period):
     interest = sum * percentage
-
     total_interest += interest
-
     sum = sum + (sum * percentage)
     print('Year', num + 1, ':', round(sum, 2))
 print()
